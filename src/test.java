98450e040a5955f2b13b1b9fa99c9969c7b3529c
Sohail Zaman <112624170+sohail-cs@users.noreply.github.com>
2024-06-26 21:37:02 +0100

    Add files via upload

---
+++ src/test.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import PICTUREPUZZLE.PICTUREPUZZLEBOARD;
+
+class test {
+
+	@Test
+	void test() {
+		PICTUREPUZZLEBOARD board = new PICTUREPUZZLEBOARD();
+	}
+
+}
